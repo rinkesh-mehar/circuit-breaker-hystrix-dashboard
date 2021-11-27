b7601257fdc2e991a604c1a7f19363ff91a06fbe
Rinkesh Mehar <40026552+rinkesh-mehar@users.noreply.github.com>
2021-11-27 11:52:15 +0530

    Implemented student service for circuit dashboard

---
+++ spring-hystrix-student-service/src/main/java/com/rinkesh/springhystrixstudentservicenew/SpringHystrixStudentServiceNewApplication.java
@@ -0,0 +1,13 @@
+package com.rinkesh.springhystrixstudentservicenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHystrixStudentServiceNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHystrixStudentServiceNewApplication.class, args);
+    }
+
+}
---
+++ spring-hystrix-student-service/src/test/java/com/rinkesh/springhystrixstudentservicenew/SpringHystrixStudentServiceNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinkesh.springhystrixstudentservicenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHystrixStudentServiceNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
